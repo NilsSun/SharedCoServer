06be90d1b50e06bc08b33a45addfa5de59696379
dtmdyzc <dtmdyzc@gmail.com>
2016-03-12 15:06:33 +0100

    Update RepositoryData.java

--- src/main/java/com/lnu/sc/data/RepositoryData.java
+++ src/main/java/com/lnu/sc/data/RepositoryData.java
@@ -1,6 +1,8 @@
 package com.lnu.sc.data;
 
 import com.lnu.sc.entities.Artifact;
+import com.lnu.sc.entities.Collection;
+
 import java.util.ArrayList;
 import java.util.List;
 
@@ -11,9 +13,15 @@ import java.util.List;
  */
 public class RepositoryData {
         private static List<Artifact> artifacts = new ArrayList<Artifact>();
-	
+        private static List<Collection> collections  = new ArrayList<Collection>();
+        
 	public static List<Artifact> getArtifact(){
 		return artifacts;
+		
+	}
+	
+	public static List<Collection> getCollection(){
+		return collections;
 		
 	}
 }
